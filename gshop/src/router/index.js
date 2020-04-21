e5170d6c8dfda329bc69a708fe37b1c0571ce9f9
smith_zuo <2546078657@qq.com>
2020-04-21 10:26:17 +0800

    新增首页提交

--- gshop/src/router/index.js
+++ gshop/src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+const MSite = () => import('@/pages/MSite/MSite.vue') // 路由懒加载
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'MSite',
+    component: MSite
+  }]
 })
--- gshop_node/routes/index.js
+++ gshop_node/routes/index.js
@@ -5,5 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/index_category', function(req, res) {
+  setTimeout(function() {
+    const data = require('../data/index_category.json')
+    res.send({ code: 0, data })
+  }, 300)
+})
 module.exports = router;
